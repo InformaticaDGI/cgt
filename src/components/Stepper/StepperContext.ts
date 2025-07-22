63d4e216f2e09fad5b1c5c746abd315855790f75
Salvador Ron <salvadorronosio@gmail.com>
2025-07-22 09:19:21 -0400

    fix stepper

---
+++ src/components/Stepper/StepperContext.ts
@@ -0,0 +1,23 @@
+import { createContext } from "react";
+
+interface StepperContextType {
+    currentStep: number;
+    isLastStep: boolean;
+    isFirstStep: boolean;
+    lastStep: number;
+    firstStep: number;
+    setStep: (step: number) => void;
+    nextStep: () => void;
+    previousStep: () => void;
+}
+
+export const StepperContext = createContext<StepperContextType>({
+    currentStep: 0,
+    setStep: () => null,
+    isLastStep: false,
+    isFirstStep: false,
+    nextStep: () => null,
+    previousStep: () => null,
+    lastStep: 0,
+    firstStep: 0
+})
