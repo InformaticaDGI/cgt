882d371d1941bc1a22ccafe50a0af8952cc978c0
Jose Segovia <jose.segovia.r@gmail.com>
2025-07-24 23:55:51 -0400

    fix filter selects by permission

--- src/store/auth-storage.ts
+++ src/store/auth-storage.ts
@@ -1,39 +1,39 @@
-import { create } from "zustand"
-import { persist } from "zustand/middleware"
+import { create } from "zustand";
+import { persist } from "zustand/middleware";
 
 interface Role {
-    id: string
-    name: string
-    policies: string[]
+  id: string;
+  name: string;
+  policies: string[];
 }
 
 interface User {
-    id: string
-    email: string
-    name: string
-    role: Role
-  }
-  
-  interface UserState {
-    user: User | null
-    token: string | null
-    isAuthenticated: boolean
-    login: (user: User, token: string) => void
-    logout: () => void
-  }
+  id: string;
+  email: string;
+  institution: string | number;
+  name: string;
+  role: Role;
+}
 
-  
-  export const useAuthStorage = create<UserState>()(
-    persist(
-      (set) => ({
-        user: null,
-        token: null,
-        isAuthenticated: false,
-        login: (user, token) => set({ user, token, isAuthenticated: true }),
-        logout: () => set({ user: null, token: null, isAuthenticated: false }),
-      }),
-      {
-        name: 'auth-storage'
-      }
-    )
+interface UserState {
+  user: User | null;
+  token: string | null;
+  isAuthenticated: boolean;
+  login: (user: User, token: string) => void;
+  logout: () => void;
+}
+
+export const useAuthStorage = create<UserState>()(
+  persist(
+    (set) => ({
+      user: null,
+      token: null,
+      isAuthenticated: false,
+      login: (user, token) => set({ user, token, isAuthenticated: true }),
+      logout: () => set({ user: null, token: null, isAuthenticated: false }),
+    }),
+    {
+      name: "auth-storage",
+    }
   )
+);
