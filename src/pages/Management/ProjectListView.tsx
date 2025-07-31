35425fbd0e1d12d660235f864f83879a91a5f67f
Salvador Ron <salvadorronosio@gmail.com>
2025-07-31 12:56:38 -0400

    fix useprojects

--- src/pages/Management/ProjectListView.tsx
+++ src/pages/Management/ProjectListView.tsx
@@ -9,13 +9,15 @@ import { Grid, GridItem } from "../../components/Layout/Grid";
 import ProjectFilter from "../../components/Prebuilt/ProjectFilter";
 import Pagination from "../../components/Prebuilt/Pagination";
 import { useSearchParams } from "react-router";
+import { useAppStore } from "../../store/store";
 
 const ProjectsListView = () => {
 
     const [searchParams] = useSearchParams();
+    const { municipalityId, parrishId, secretaryParentId, secretarialTerritoryId  } = useAppStore()
     const page = searchParams.get('page') ? parseInt(searchParams.get('page') ?? '') : 1;
     const limit = searchParams.get('limit') ? parseInt(searchParams.get('limit') ?? '') : 10;
-    const { data: { data: projects, pagination } } = useProjects({ page, limit });
+    const { data: { data: projects, pagination } } = useProjects({ page, limit, include: 'parish.municipality,secretary', parishId: parrishId || undefined, municipalityId: municipalityId || undefined, secretaryId: secretaryParentId || undefined, territorialSecretaryId: secretarialTerritoryId || undefined });
 
     return (
         <Flex $height='100%' $direction="column" $width='85vw' $justify='center' $align='stretch' $padding='1rem' style={{ position: 'relative' }}>
