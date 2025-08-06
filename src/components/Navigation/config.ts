d04df4352e480ce5947039f82f1015c9ed7348b1
Salvador Ron <salvadorronosio@gmail.com>
2025-08-06 15:06:38 -0400

    fix test

--- src/components/Navigation/config.ts
+++ src/components/Navigation/config.ts
@@ -6,12 +6,13 @@ import { IoIosExit } from "react-icons/io";
 import authRoles from "./authRoles.ts"
 
 
- createNavItems: (logout: () => void)  NavItems = (logout: () => void) => [
+const createNavItems: (logout: () => void) => NavItems = (logout: () => void) => [
         {
             id: 'map',
             to: '/mapa',
             label: 'Mapa',
             icon: FaMapLocationDot,
+            roles: authRoles.Public
         },
         {
             id: 'dashboard',
